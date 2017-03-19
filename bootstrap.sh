64c09fabcf1a1a77ff27a2a379bc49e05d942d86
Sudokamikaze <pulshencode@outlook.com>
2017-03-19 20:18:22 +0200

    Some rework

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,8 @@
-#!/bin/bash
 
-echo -n "This script install buildscripts into this dir, do you want to processed? [Y/N]: "
-read choise
-case "$choise" in
-  y|Y) git clone https://github.com/Sudokamikaze/Darkspell-scripts.git
-  rm Darkspell-scripts/LICENSE
-  rm Darkspell-scripts/README.md
-  rm Darkspell-scripts/bootstrap.sh
-  mv Darkspell-scripts/* ./
-  rm -rf Darkspell-scripts
-  echo "Done! "
-  ;;
-  n|N)
-  exit 1
-  ;;
-esac
+git clone https://github.com/Sudokamikaze/Darkspell-scripts.git
+rm Darkspell-scripts/LICENSE
+rm Darkspell-scripts/README.md
+rm Darkspell-scripts/bootstrap.sh
+mv Darkspell-scripts/* ./
+rm -rf Darkspell-scripts
+echo "Done! "
