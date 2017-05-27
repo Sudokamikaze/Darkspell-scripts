56c5a3a801123eb558625a09e1a6ae3f6b36cc9b
Sudokamikaze <pulshencode@outlook.com>
2017-05-27 15:23:30 +0300

    Rewrited zip creator, added support for new flasher type

--- zip_creator.sh
+++ zip_creator.sh
@@ -4,45 +4,48 @@ eval $(grep DEVICE= ./config.buildscripts)
 eval $(grep CONFIG_LOCALVERSION= ./arch/arm/configs/sudokamikaze_"$DEVICE"_defconfig)
 eval $(grep VER= ./config.buildscripts)
 eval $(grep BRANCH= ./config.buildscripts)
+eval $(grep FLASHER= ./config.buildscripts)
 DATE=$(date +%d-%m-%Y)
 
-rm -rf Darkspell-Flasher-*
-git clone https://github.com/Sudokamikaze/Darkspell-Flasher-"$DEVICE".git -b "$BRANCH" && cd Darkspell-Flasher-"$DEVICE"
+function darkspell_flasher {
+  git clone https://github.com/Sudokamikaze/Darkspell-Flasher-"$DEVICE".git -b "$BRANCH" && cd Darkspell-Flasher-*
 
-case "$DEVICE" in
-  taoshan|mako)
-cp ../arch/arm/boot/zImage tools/
-cp ../drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini system/etc/firmware/wlan/prima/
-  ;;
-  grouper)
-  cp ../arch/arm/boot/zImage tools/
-  ;;
+  if [ "$VER" == "LP" ]; then
+  cp ../modules_dir/*.ko system/lib/modules
+  fi
+
+  case "$DEVICE" in
+    taoshan)
+    cp ../arch/arm/boot/zImage tools/
+    cp ../drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini system/etc/firmware/wlan/prima/
+    ;;
+    grouper)
+    cp ../arch/arm/boot/zImage tools/
+    ;;
 esac
+zip Darkspell.zip -r *
 
-function zipcreate {
- case "$DEVICE" in
-   taoshan|mako)
-   if [ "$VER" == "LP" ]; then
-   cp ../modules_dir/*.ko system/lib/modules
- fi
-   zip Darkspell-Stable.zip -r META-INF presets system tools
-   ;;
-   grouper)
-   zip Darkspell-Stable.zip -r META-INF tools
-   ;;
-esac
-}
-
-function sign {
-  mv Darkspell-Stable.zip signer/
-  echo Signing zip file
-  cd signer && java -jar signapk.jar testkey.x509.pem testkey.pk8 Darkspell-Stable.zip Darkspell-Stable-signed.zip
-  rm Darkspell-Stable.zip
-  mv Darkspell-Stable-signed.zip ../$DATE$CONFIG_LOCALVERSION-$VER-$DEVICE.zip
-  cd ..
-}
-
-zipcreate
-sign
-echo "You may grab zip file from Darkspell-Flasher directory"
+mv Darkspell-Stable.zip signer/
+cd signer && java -jar signapk.jar testkey.x509.pem testkey.pk8 Darkspell.zip Darkspell-Stable-signed.zip
+rm Darkspell.zip
+mv Darkspell-Stable-signed.zip ../$VER$CONFIG_LOCALVERSION-$DEVICE-$DATE.zip
 cd ..
+echo "Done, grab your file in flasher directory"
+}
+
+function anykernel_flasher {
+git clone git@github.com:Sudokamikaze/AnyKernel2_mako.git -b $BRANCH && cd AnyKernel2_mako
+cp arch/arm/boot/zImage ./
+zip Sinai.zip -r *
+mv Sinai.zip $VER$CONFIG_LOCALVERSION-$DEVICE-$DATE.zip
+echo "Done, grab your file in flasher directory"
+}
+
+case "$FLASHER" in
+  anykernel) anykernel_flasher
+  exit
+  ;;
+  darkspell) darkspell_flasher
+  exit
+  ;;
+esac
