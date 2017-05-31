38b44f4a6a236e182c6ddd79d1366dca2bf0fdbf
Sudokamikaze <pulshencode@outlook.com>
2017-05-31 16:38:38 +0300

    Also cleanup anykernel directory

--- clean.sh
+++ clean.sh
@@ -10,6 +10,7 @@ rm -rf $KERNEL_DIR/ramdisk.cpio
 rm -rf $KERNEL_DIR/root.fs
 rm -rf $KERNEL_DIR/boot.img
 rm -rf Darkspell-Flasher-*
+rm -rf AnyKernel2_mako
 if [ "$VER" == "LP" ]; then
 rm -rf $KERNEL_DIR/modules_dir/*
 fi
