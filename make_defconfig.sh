25f404c292e76b6ea245a0ce9d3a182195dfd694
sudokamikaze <pulshencode@outlook.com>
2017-06-14 18:27:05 +0300

    Move sudokamikaze to variable

--- make_defconfig.sh
+++ make_defconfig.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 ITSME=$(git config user.name)
 eval $(grep DEVICE= ./config.buildscripts)
+N=sudokamikaze
 
 export ARCH=arm
 export SUBARCH=arm
-make sudokamikaze_"$DEVICE"_defconfig
+make "$N"_"$DEVICE"_defconfig
 make menuconfig
-make savedefconfig && cp defconfig arch/arm/configs/sudokamikaze_"$DEVICE"_defconfig
+make savedefconfig && cp defconfig arch/arm/configs/"$N"_"$DEVICE"_defconfig
 rm defconfig
 
-if [ $ITSME == Sudokamikaze ]; then
+case "$ITSME" in
+  Sudokamikaze | sudokamikaze)
 echo -n "Do you wan't to commit? [Y/N]: "
 read douwant
 case "$douwant" in
   y|Y)
 echo -n "defconfig: "
 read commitmsg
-git add arch/arm/configs/sudokamikaze_"$DEVICE"_defconfig
+git add arch/arm/configs/"$N"_"$DEVICE"_defconfig
 git commit -S -m "defconfig: $commitmsg"
 ;;
   n|N) exit
 ;;
 esac
-fi
+;;
+esac
