4dbd4762863f7fc188417fa64cc7de745a50c19c
sudokamikaze <sudokamikaze@protonmail.com>
2018-03-17 23:10:37 +0200

    Remove cleantc dependency
    Signed-off-by: sudokamikaze <sudokamikaze@protonmail.com>

--- clean.sh
+++ clean.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-eval $(grep CLEAN_TOOLCHAIN_PATH= ./config.buildscripts)
+eval $(grep TOOLCHAIN_PATH= ./config.buildscripts)
 eval $(grep VER= ./config.buildscripts)
 
 DIR=$(pwd)
 KERNEL_DIR="$DIR"
-make ARCH=arm CROSS_COMPILE="$CLEAN_TOOLCHAIN_PATH/bin/arm-linux-gnueabihf-" -j5 clean mrproper
+make ARCH=arm CROSS_COMPILE="$TOOLCHAIN_PATH/bin/arm-eabi-" -j5 clean mrproper
 rm -rf $KERNEL_DIR/ramdisk.cpio
 rm -rf $KERNEL_DIR/root.fs
 rm -rf $KERNEL_DIR/boot.img
